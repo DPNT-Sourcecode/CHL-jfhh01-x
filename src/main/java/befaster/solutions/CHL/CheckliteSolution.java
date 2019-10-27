a12580b88df8bedd811cd81b3f6f0b9ffd468777
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-10-27 14:33:09 +0000

    Sun Oct 27 14:33:09 GMT 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -13,25 +13,29 @@ public class CheckliteSolution {
     	itemMap.put("B", 0);
     	itemMap.put("C", 0);
     	itemMap.put("D", 0);
+    	itemMap.put("E", 0);
     	
     	for(char item:skus.toCharArray()) {
     		switch(item){
     			case 'A': itemMap.put("A",itemMap.get("A")+1); break;
 	    		case 'B': itemMap.put("B",itemMap.get("B")+1); break;
 	    		case 'C': itemMap.put("C",itemMap.get("C")+1); break;
-	    		case 'D': itemMap.put("D",itemMap.get("D")+1); break;   		
+	    		case 'D': itemMap.put("D",itemMap.get("D")+1); break;  
+	    		case 'E': itemMap.put("E",itemMap.get("E")+1); break;  		
 	    		default: return -1;
     		}
     	}
     	int totalCheckout = 0;
     	
     	for(String key:itemMap.keySet()) {
-    		totalCheckout+=calculatePriceOfItem(key,itemMap.get(key));
+    		//totalCheckout+=calculatePriceOfItemR1(key,itemMap.get(key));
+    		
+    		totalCheckout+=calculatePriceOfItemR2(key,itemMap.get(key));
     	}
     	return totalCheckout;
     }
     
-    // Method to calculate price of items in basket by applying special offers
+    // Method to calculate price of items in basket by applying special offers in round 1
     public int calculatePriceOfItemR1(String item, int amount) {
     	switch(item) {
 	    	case "A": return (amount/3)*130 + (amount%3)*50;
@@ -40,5 +44,17 @@ public class CheckliteSolution {
 	    	default: return amount*15;
     	}
     }
+    
+    // Method to calculate price of items in basket by applying special offers in round 1
+    public int calculatePriceOfItemR2(String item, int amount) {
+    	switch(item) {
+	    	case "A": return (amount/3)*130 + (amount%3)*50;
+	    	case "B": return (amount/2)*45 + (amount%2)*30;
+	    	case "C": return amount*20;
+	    	case "D": return amount*20;
+	    	default: return amount*15;
+    	}
+    }
 }
+
 
