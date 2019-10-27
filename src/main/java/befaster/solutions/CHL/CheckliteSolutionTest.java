d79d8a5499b90649eaadd633ad4048823a280f87
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-10-27 15:00:09 +0000

    Sun Oct 27 15:00:09 GMT 2019

---
+++ src/main/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -0,0 +1,20 @@
+package befaster.solutions.CHL;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CheckliteSolutionTest {
+
+	@Test
+	public void testChecklite() {
+		CheckliteSolution testBasket = new CheckliteSolution();
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculatePriceOfItemR2() {
+		fail("Not yet implemented");
+	}
+
+}
