0b52bd736d90cb3df89d2088177ed587d212bee2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-10-27 18:44:35 +0000

    Sun Oct 27 18:44:35 GMT 2019

--- src/test/java/befaster/solutions/CHL/CheckLiteTest.java
+++ src/test/java/befaster/solutions/CHL/CheckLiteTest.java
@@ -11,8 +11,10 @@ public class CheckLiteTest {
 		CheckliteSolution test = new CheckliteSolution();
 		Integer expected = 155;
 		Integer actual = test.checklite("EBEBBB");
+		System.out.print(actual);
 		assertEquals("Actual: "+actual.toString(),expected, actual);
 	}
 }
+
 
 
